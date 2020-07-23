00203ba220a61e2fbdfcd53c8b604a73ef603447
tanvir5230 <tanvirbintouhid5230@gmail.com>
2020-07-24 00:48:17 +0600

    increase and decrese function set up with new concept

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
-let unitQuantityContainer = document.getElementsByTagName("input")[0];
 let itemPriceContainer = document.getElementsByTagName("h5")[1];
 let itemPrice = parseInt(itemPriceContainer.innerText.replace("$", ""));
 let unitPrice = itemPrice;
 let plusSign = document.getElementsByClassName("fa-plus")[0].parentElement;
 let minusSign = document.getElementsByClassName("fa-minus")[0].parentElement;
-let unitValue = unitQuantityContainer.value;
-unitValue = parseInt(unitValue);
+let unitValue;
+let unitValueContainer;
 function itemPriceIncrease() {
   itemPrice = unitValue * unitPrice;
   itemPriceContainer.innerText = "$" + itemPrice;
 }
+
+let productNumber = document.getElementsByClassName("product").length;
+for (let i = 0; i < productNumber; i++) {
+  document
+    .getElementsByClassName("fa-plus")
+    [i].parentElement.addEventListener("click", increase);
+  document
+    .getElementsByClassName("fa-minus")
+    [i].parentElement.addEventListener("click", decrease);
+}
+
 function increase() {
+  unitValueContainer = this.previousElementSibling;
+  unitValue = parseInt(unitValueContainer.value);
   unitValue += 1;
-  unitQuantityContainer.value = unitValue;
-  itemPriceIncrease();
+  unitValueContainer.value = unitValue;
+  event.stopPropagation();
 }
 function decrease() {
+  unitValueContainer = this.nextElementSibling;
+  unitValue = parseInt(unitValueContainer.value);
   if (unitValue > 0) {
     unitValue -= 1;
-    unitQuantityContainer.value = unitValue;
-    itemPriceIncrease();
+    unitValueContainer.value = unitValue;
   }
+  event.stopPropagation();
 }
 function removeItem() {
   this.parentElement.parentElement.parentElement.remove();
