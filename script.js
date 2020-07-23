5a05c6fe2f8569809e47529696fc7a08bea81bbe
tanvir5230 <tanvirbintouhid5230@gmail.com>
2020-07-23 23:48:38 +0600

    increasePrice function and removeItem function added

--- script.js
+++ script.js
@@ -6,17 +6,27 @@ let plusSign = document.getElementsByClassName("fa-plus")[0].parentElement;
 let minusSign = document.getElementsByClassName("fa-minus")[0].parentElement;
 let unitValue = unitQuantityContainer.value;
 unitValue = parseInt(unitValue);
+function itemPriceIncrease() {
+  itemPrice = unitValue * unitPrice;
+  itemPriceContainer.innerText = "$" + itemPrice;
+}
 function increase() {
   unitValue += 1;
   unitQuantityContainer.value = unitValue;
-  itemPrice = unitValue * unitPrice;
-  itemPriceContainer.innerText = "$" + itemPrice;
+  itemPriceIncrease();
 }
 function decrease() {
   if (unitValue > 0) {
     unitValue -= 1;
     unitQuantityContainer.value = unitValue;
+    itemPriceIncrease();
   }
+}
+function removeItem() {
+  this.parentElement.parentElement.parentElement.remove();
 }
 plusSign.addEventListener("click", increase);
 minusSign.addEventListener("click", decrease);
+
+let crossBtn = document.getElementsByClassName("remove-item")[0];
+crossBtn.addEventListener("click", removeItem);
